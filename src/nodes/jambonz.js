41931a6e87fc3e7eb39b5ff4ea05e6127360138a
Dave Horton <daveh@beachdognet.com>
2020-10-08 13:57:20 -0400

    add create sms node (REST)

--- src/nodes/jambonz.js
+++ src/nodes/jambonz.js
@@ -570,6 +570,13 @@ module.exports = function(RED) {
     return post(`Accounts/${accountSid}/Calls`, opts);
   }
 
+  function doCreateMessage(baseUrl, accountSid, apiToken, opts) {
+    const post = bent(`${baseUrl}/v1/`, 'POST', 'json', 201, {
+      'Authorization': `Bearer ${apiToken}`
+    });
+    return post(`Accounts/${accountSid}/Messages`, opts);
+  }
+
 
   /** LCC */
   function lcc(config) {
@@ -729,6 +736,61 @@ module.exports = function(RED) {
     });
   }
   RED.nodes.registerType('create-call', create_call);
+
+  /** Create sms */
+  function create_sms(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    const server = RED.nodes.getNode(config.server);
+
+    node.on('input', async(msg, send, done) => {
+      send = send || function() { node.send.apply(node, arguments);};
+
+      const {url, accountSid, apiToken} = server.credentials;
+      if (!url || !accountSid || !apiToken) {
+        node.log(`invalid / missing credentials, skipping create-message node: ${JSON.stringify(server.credentials)}`);
+        send(msg);
+        if (done) done();
+        return;
+      }
+
+      var from = v_resolve(config.from, config.fromType, this.context(), msg);
+      var to = v_resolve(config.to, config.toType, this.context(), msg);
+      var text = v_resolve(config.text, config.textType, this.context(), msg);
+      var provider = v_resolve(config.provider, config.providerType, this.context(), msg);
+
+      const opts = {
+        from,
+        to,
+        text,
+        provider
+      };
+
+      try {
+        node.log(`sending create message ${JSON.stringify(opts)}`);
+        const res = await doCreateMessage(url, accountSid, apiToken, opts);
+        msg.statusCode = 202;
+        msg.messageSid = res.sid;
+        msg.providerResponse = res.providerResponse;
+        node.log(`successfully launched call with messageSid ${msg.messageSid}`);
+      } catch (err) {
+        if (err.statusCode) {
+          node.log(`create_message failed with ${err.statusCode}`);
+          msg.statusCode = err.statusCode;
+        }
+        else {
+          node.log(`Error sending create message ${JSON.stringify(err)}`);
+          if (done) done(err);
+          else node.error(err, msg);
+          send(msg);
+          return;
+        }
+      }
+      send(msg);
+      if (done) done();
+    });
+  }
+  RED.nodes.registerType('create-sms', create_sms);
 };
 
 // helper functions
