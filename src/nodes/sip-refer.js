2cd04e6db75fc46f952f39866d4c572cc94928e5
Sam Machin <sam@sammachin.com>
2022-12-05 16:23:43 +0000

    sip:refer & sip:request nodes

--- src/nodes/sip-refer.js
+++ src/nodes/sip-refer.js
@@ -1,36 +1,33 @@
 var {createHash} = require('crypto');
-const bent = require('bent');
 var mustache = require('mustache');
 mustache.escape = function(text) {return text;};
-var {appendVerb, v_resolve} = require('./libs');
-const { type } = require('os');
-const { ConfigurationOptions } = require('aws-sdk');
+var {appendVerb, } = require('./libs');
 
 module.exports = function(RED) {
-  /** sip:decline */
-  function sip_request(config) {
+  function sip_refer(config) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on('input', function(msg) {
-      obj = {
-        verb: 'sip:request',
-        method: config.method
-      }
       Object.keys(config).forEach(function(key, index) {
         if (config[key] == ''){
           config[key] = false
         };
       });
+      obj = {
+        verb: 'sip:refer',
+        referTo: config.referTo
+      }
       if (typeof(config.headers == 'string')){
         config.headers=JSON.parse(config.headers)
       }
       config.headers ? obj.headers = config.headers : null
-      config.body ? obj.body = config.body : null
+      config.referredBy ? obj.referredBy = config.referredBy : null
       config.actionHook ? obj.actionHook = config.actionHook : null
+      config.eventHook ? obj.eventHook = config.eventHook : null
       appendVerb(msg, obj)
       node.send(msg);
     });
   }
-  RED.nodes.registerType('sip:request', sip_request);
+  RED.nodes.registerType('sip:refer', sip_refer);
 
 }
