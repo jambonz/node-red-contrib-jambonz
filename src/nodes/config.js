f8c0d033db2cbd45f42890e782257fc729fcb34c
Sam Machin <sam@sammachin.com>
2022-12-07 12:05:09 +0000

    finished config node

--- src/nodes/config.js
+++ src/nodes/config.js
@@ -49,7 +49,23 @@ module.exports = function(RED) {
             config.bargeIn_maxDigits != '' ? obj.bargeIn.maxDigits = config.bargeIn_maxDigits : null
             config.bargeIn_interDigitTimeout != '' ? obj.bargeIn.interDigitTimeout = config.bargeIn_interDigitTimeout : null
           }
-          console.log(obj)
+
+          if (config.amd){
+            obj.amd = {timers : {}}
+            config.amd_actionHook != '' ? obj.amd.actionHook = config.amd_actionHook : null
+            config.amd_thresholdWordCount != '' ? obj.amd.thresholdWordCount = config.amd_thresholdWordCount : null
+            config.amd_timers_noSpeechTimeoutMs != '' ? obj.amd.timers.noSpeechTimeoutMs = config.amd_timers_noSpeechTimeoutMs : null
+            config.amd_timers_decisionTimeoutMs != '' ? obj.amd.timers.decisionTimeoutMs = config.amd_timers_decisionTimeoutMs : null
+            config.amd_timers_toneTimeoutMs != '' ?	 obj.amd.timers.toneTimeoutMs = config.amd_timers_toneTimeoutMs : null
+            config.amd_timers_greetingCompletionTimeoutMs != '' ? obj.amd.timers.greetingCompletionTimeoutMs = config.amd_timers_greetingCompletionTimeoutMs : null
+          }
+
+          if (config.record){
+            obj.record = {}
+            config.record_action != '' ? obj.record.action = config.record_action : null //TODO if multiple, split into array
+            config.record_siprecServerURL != '' ? obj.record.siprecServerURL = config.record_siprecServerURL : null
+            config.record_recordingID != '' ? obj.record.recordingID = config.record_recordingID : null
+          }
           appendVerb(msg,  obj);
           node.send(msg);
         });
