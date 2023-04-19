842b0430beee321c0363d0fa8b626e1cdbf333b5
Anton Voylenko <anton.voylenko@gmail.com>
2023-04-19 19:53:02 +0300

    support filtering for get_calls node

--- src/nodes/get_calls.js
+++ src/nodes/get_calls.js
@@ -1,4 +1,5 @@
 const bent = require("bent");
+var { v_resolve } = require("./libs");
 
 module.exports = function (RED) {
   function get_calls(config) {
@@ -7,10 +8,25 @@ module.exports = function (RED) {
     const server = RED.nodes.getNode(config.server);
     const { accountSid, apiToken } = server.credentials;
     node.on("input", async (msg, send, done) => {
+      const data = {
+        direction: v_resolve(
+          config.direction,
+          config.directionType,
+          this.context(),
+          msg
+        ),
+        from: v_resolve(config.from, config.fromType, this.context(), msg),
+        to: v_resolve(config.to, config.toType, this.context(), msg),
+        callStatus: v_resolve(config.callStatus, config.callStatusType, this.context(), msg),
+      };
+      Object.keys(data).forEach(
+        (k) => data[k] == null || (data[k] == '' && delete data[k])
+      );
+      const params = new URLSearchParams(data).toString();
       const req = bent(
-        `${server.url}/v1/Accounts/${accountSid}/Calls`,
-        "GET",
-        "json",
+        `${server.url}/v1/Accounts/${accountSid}/Calls${ params ? '?' + params : ''}`,
+        'GET',
+        'json',
         {
           Authorization: `Bearer ${apiToken}`,
         }
