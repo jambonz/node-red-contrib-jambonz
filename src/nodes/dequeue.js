bb0a5a44375b9a860f48b3f64ba6dd0288a187d5
Anton Voylenko <anton.voylenko@gmail.com>
2023-06-02 16:03:37 +0300

    fix timeout payload for dequeue node (#33)

--- src/nodes/dequeue.js
+++ src/nodes/dequeue.js
@@ -6,6 +6,7 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on('input', function(msg, send, done) {
+      const timeout = v_resolve(config.timeout, config.timeoutType, this.context(), msg);
       appendVerb(msg, {
         verb: 'dequeue',
         name:  v_resolve(config.queue, config.queueType, this.context(), msg),
@@ -13,7 +14,7 @@ module.exports = function(RED) {
         beep: config.beep,
         actionHook: v_resolve(config.actionHook, config.actionHookType, this.context(), msg),
         confirmHook: v_resolve(config.confirmHook, config.confirmHookType, this.context(), msg),
-        timeout:  v_resolve(config.timeout, config.timeoutType, this.context(), msg)
+        timeout: (/^\d+$/.test(timeout)) ? parseInt(timeout) : null,
       });
       node.send(msg);
     });
