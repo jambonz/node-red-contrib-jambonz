bd607f4a67cfc9e233ded28bc980b583adc6f610
Anton Voylenko <anton.voylenko@gmail.com>
2023-08-23 17:50:09 +0300

    improve conference node (#54)

--- src/nodes/conference.js
+++ src/nodes/conference.js
@@ -6,17 +6,20 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on('input', function(msg) {
-      var maxParticipants = new_resolve(RED, config.maxParticipants, config.maxParticipantsType, node, msg)
+      var statusHook = new_resolve(RED, config.statusHook, config.statusHookType, node, msg);
       appendVerb(msg, {
         verb: 'conference',
         name: new_resolve(RED, config.conference, config.conferenceType, node, msg),
         enterHook: new_resolve(RED, config.enterHook, config.enterHookType, node, msg),
         waitHook: new_resolve(RED, config.waitHook, config.waitHookType, node, msg),
-        maxParticipants,
+        actionHook: new_resolve(RED, config.actionHook, config.actionHookType, node, msg),
+        statusHook,
+        ...(statusHook && { statusEvents: ['start', 'end', 'join', 'leave'] }),
+        maxParticipants: new_resolve(RED, config.maxParticipants, config.maxParticipantsType, node, msg),
         beep: config.beep,
         startConferenceOnEnter: config.startConferenceOnEnter,
-        endOnExit: config.endOnExit,
-        joinMuted : config.joinMuted
+        endConferenceOnExit: config.endConferenceOnExit,
+        joinMuted: config.joinMuted
       });
       node.send(msg);
     });
