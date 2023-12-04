a8e14b251f3a6531719126946ba2082292ac57d6
Anton Voylenko <anton.voylenko@gmail.com>
2023-12-04 03:03:11 +0200

    Sip decline headers (#74)
    * check content type on llc
    
    * sip:decline support custom headers

--- src/nodes/sip-decline.js
+++ src/nodes/sip-decline.js
@@ -8,11 +8,16 @@ module.exports = function(RED) {
     node.on('input', function(msg) {
       var status = new_resolve(RED, config.status, config.statusType, node, msg);
       var reason = new_resolve(RED, config.reason, config.reasonType, node, msg);
-      appendVerb(msg, {
+      var obj = {
         verb: 'sip:decline',
         status: parseInt(status),
         reason
-      });
+      }
+      config.headers ? obj.headers = new_resolve(RED, config.headers, config.headersType, node, msg) : null
+      if (typeof obj.headers == 'string'){
+        obj.headers = JSON.parse(obj.headers)
+      }
+      appendVerb(msg, obj);
       node.send(msg);
     });
   }
