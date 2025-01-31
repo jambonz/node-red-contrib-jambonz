e7ec271fdd39728e68ab0adc54e1a78a00da1784
Anton Voylenko <anton.voylenko@gmail.com>
2025-01-31 19:12:47 +0200

    fix: clone object and fix possible crash (#100)

--- src/nodes/tag.js
+++ src/nodes/tag.js
@@ -6,8 +6,15 @@ module.exports = function(RED) {
     var node = this;
     node.on('input', async function(msg) {
       var data = await new_resolve(RED, config.data, config.dataType, node, msg);
-      if (typeof(data) != 'object'){
-        data = JSON.parse(data)
+      if (typeof(data) != 'object') {
+        try {
+          data = JSON.parse(data)
+        } catch (e) {
+          console.log('Failed to parse data as JSON: ', data)
+          return node.send(msg);
+        }
+      } else {
+        data = Object.assign({}, data);
       }
       appendVerb(msg, {
         verb: 'tag',
